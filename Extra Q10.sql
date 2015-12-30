0ae6c1190da6955672d4868f7f6b52e1690dba97
cuzntone <cuzntone@users.noreply.github.com>
2015-12-30 15:18:37 -0800

    Create Extra Q10.sql

---
+++ Extra Q10.sql
@@ -0,0 +1,12 @@
+
+/****Find the movie(s) with the highest average rating. Return the movie title(s) and average rating. 
+(Hint: This query is more difficult to write in SQLite than other systems; 
+you might think of it as finding the highest average rating and then choosing the movie(s) with that average rating.) ****/
+
+select title, a.average as highest 
+from (select mID, avg(stars) as average 
+      from rating group by mID) a, movie
+      where movie.mID=a.mID and a.average = 
+      (select max(b.average) 
+      from (select mID, avg(stars) as average from rating group by mID) b
+      );
